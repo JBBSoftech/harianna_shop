de71d21bc7fa680799cbf6cbd6dab7ade11b79e7
JBBSoftech <jbbsoftech@gmail.com>
2025-11-22 14:55:20 +0530

    Update Appifyours/frontend/lib/main.dart

--- Appifyours/frontend/lib/main.dart
+++ Appifyours/frontend/lib/main.dart
@@ -964,7 +964,7 @@ class _HomePageState extends State<HomePage> {
                             _filterProducts(value);
                           },
                           decoration: InputDecoration(
-                            hintText: 'Rajkumar',
+                            hintText: 'vasanth',
                             prefixIcon: const Icon(Icons.search),
                             suffixIcon: const Icon(Icons.filter_list),
                             border: OutlineInputBorder(
